f0002de9003c3c246d9f16e2a5dc6e45181e7718
AJAY2405 <rahul372602@gmail.com>
2026-02-18 18:18:05 +0530

    Added gunicorn for Render

--- app.py
+++ app.py
@@ -474,5 +474,8 @@ def reset_all():
     return jsonify({"reset": True, "message": "System completely reset - start fresh"})
 
 # ---------------- run ------------------------
+# if __name__ == "__main__":
+#     app.run(debug=True)
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
